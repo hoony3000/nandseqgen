b10394dae7cf41acb4b3a90a4669ca3f53f3d7fb
hoony3000 <hoony3000@gmail.com>
2025-07-28 11:35:12 +0900

    Create gen_seq.py

---
+++ gen_seq.py
@@ -0,0 +1,34 @@
+NS = 1
+US = 10**3
+MS = 10**6
+S = 10**9
+
+import bisect
+from typing import Union, List, Tuple, TypeVar, Generic, Callable, Any, Set
+import itertools
+import hashlib
+import threading
+import weakref
+from dataclasses import dataclass, field
+import numpy as np
+import yaml
+import os
+
+# 중복 instance 생성 방지를 위한 parent class 정의
+class DeduplicateBase
+  """
+  multi-thread 환경(필요하다면)에서 instance 중복 생성 방지를 위한 id, lock, counter
+  """
+  _global_lock = threading.Lock()
+  _global_instances = weakref.WeakValueDictionary()
+  _class_counters = {}
+  _class_instances_by_id = {}
+
+  @classmethod
+  def create(cls, *args, force_new=False, **kwargs):
+    """
+    instance 생성자
+    """
+    key = cls._make_key(*args, **kwargs)
+    if not force_new:
+      with cls._global_lock
