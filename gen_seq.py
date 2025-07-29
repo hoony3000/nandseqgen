f1498ff93f80a7f36669bd5b98679480c8dfe514
hoony3000 <hoony3000@gmail.com>
2025-07-29 18:29:41 +0900

    Update gen_seq.py

--- gen_seq.py
+++ gen_seq.py
@@ -810,4 +810,98 @@ class AddressManager:
     return self.size
 
   def get_adds_erasable(self):
-    return np.where(self.adds != -4)
+    return np.where(self.adds != -4)[0]
+
+  def get_adds_pgmable(self):
+    blkadds = np.where(self.adds >= -1)[0]
+    return np.array(tuple(zip(blkadds, self.adds[blkadds]+1)))
+
+  def get_adds_readable(self, offset: int = None):
+    try:
+      if(offset):
+        _offset = offset
+      else:
+        _offset = self.readoffset
+    except ValueError:
+      print(f"offset 값 오류")
+
+    blkadds = np.where( (self.adds >= -2) & (self.adds >= _offset) )[0]
+    readadds = self.adds[blkadds]
+    readadds -= _offset
+
+    arr_tot = []
+    for idx, blkadd in enumerate(blkadds):
+      arr = [ (blkadd, readadd) for readadd in range(readadds[idx]+1) ]
+      arr_tot.extend(arr)
+
+    return np.array(arr_tot)
+
+  def sample_erasable(self, num: int):
+    arr = self.get_adds_erasable()
+    idx = np.random.choice(len(arr), size=num, replace=Fase)
+    return arr[idx]
+
+  def sample_pgmable(self, num: int):
+    arr = self.get_adds_pgmable()
+    idx = np.random.choice(len(arr), size=num, replace=Fase)
+    return arr[idx]
+
+  def sample_readable(self, num: int, offset: int = None):
+    try:
+      if(offset):
+        arr = self.get_adds_readable(offset)
+      else:
+        arr = self.get_adds_readable()
+    except ValueError:
+      print(f"offset 값 오류")
+
+    idx = np.random.choice(len(arr), size=num, replace=False)
+    return arr[idx]
+
+TimeSpec = {}
+ExecTime = {}
+
+# yaml 파일 load 하여 AC timing load
+with open('timespecs.yaml', 'r', encoding='utf-8') as file:
+  Dict = yaml.safe_load(file)
+  TimeSpec = Dict['TimeSpec']
+  ExecTime = Dict['ExecTime']
+
+# TimeSpec 정의 (수정중)
+tWB                        = StateSeq.create(times=TimeSpec['tWB']             ,states='tWB'                        )
+tERS_TLC                   = StateSeq.create(times=TImeSpec['tERS_TLC']        ,states='erase TLC busy'             )
+tPROGO                     = StateSeq.create(times=TImeSpec['tPROGO']          ,states='pgm TLC busy'               )
+tTRAN_L                    = StateSeq.create(times=TImeSpec['tTRAN']           ,states='pgm LSB transfer busy'      )
+tTRAN_C                    = StateSeq.create(times=TImeSpec['tTRAN']           ,states='pgm CSB transfer busy'      )
+tTRAN_M                    = StateSeq.create(times=TImeSpec['tTRAN']           ,states='pgm MSB transfer busy'      )
+tR_SLC                     = StateSeq.create(times=TImeSpec['tR_SLC']          ,states='read SLC busy'              )
+tR_LSB                     = StateSeq.create(times=TImeSpec['tR_LSB']          ,states='read TLC busy'              )
+tR_CSB                     = StateSeq.create(times=TImeSpec['tR_CSB']          ,states='read TLC busy'              )
+tR_MSB                     = StateSeq.create(times=TImeSpec['tR_MSB']          ,states='read TLC busy'              )
+tRRC_SLC                   = StateSeq.create(times=TImeSpec['tRRC']            ,states='read SLC extready'          )
+tRRC_FWSLC                 = StateSeq.create(times=TImeSpec['tRRC']            ,states='read FWSLC extready'        )
+tRRC_TLC                   = StateSeq.create(times=TImeSpec['tRRC']            ,states='read TLC extready'          )
+tRST_ready                 = StateSeq.create(times=TImeSpec['tRST_ready']      ,states='reset busy ready'           )
+tRST_read                  = StateSeq.create(times=TImeSpec['tRST_read']       ,states='reset busy read'            )
+tRST_pgm                   = StateSeq.create(times=TimeSpec['tRST_pgm']        ,states='reset busy pgm'             )
+tERSL_TLC                  = StateSeq.create(times=TImeSpec['tRST_pgm']        ,states='erase TLC suspend busy'     )
+tPGMSL_TLC                 = StateSeq.create(times=TImeSpec['tRST_ready']      ,states='program TLC suspend busy'   )
+
+# operations 정의 (수정중)
+# seq[0] : operation 의 execution time -> 추후 dictionary data 로 입력을 받을 예정
+name='erase TLC'            ; erase_TLC                = Operation.create(name=name  ,seq=Nop(ExecTime[name]) + tWB + tERS_TLC              + End(name)          ,applyto='die'    )
+name='pgm LSB'              ; pgm_LSB                  = Operation.create(name=name  ,seq=Nop(ExecTime[name])                               + End(name)          ,applyto='die'    )
+name='pgm CSB'              ; pgm_CSB                  = Operation.create(name=name  ,seq=Nop(ExecTime[name])                               + End(name)          ,applyto='die'    )
+name='pgm MSB'              ; pgm_MSB                  = Operation.create(name=name  ,seq=Nop(ExecTime[name])                               + End(name)          ,applyto='die'    )
+name='pgm MSB exec'         ; pgm_MSB_exec             = Operation.create(name=name  ,seq=Nop(ExecTime[name])                               + End(name)          ,applyto='die'    )
+name='read LSB'             ; read_LSB                 = Operation.create(name=name  ,seq=Nop(ExecTime[name]) + tR_LSB + tRRC_TLC           + End('read TLC')    ,applyto='die'    )
+name='read CSB'             ; read_CSB                 = Operation.create(name=name  ,seq=Nop(ExecTime[name]) + tR_CSB + tRRC_TLC           + End('read TLC')    ,applyto='die'    )
+name='read MSB'             ; read_MSB                 = Operation.create(name=name  ,seq=Nop(ExecTime[name]) + tR_MSB + tRRC_TLC           + End('read TLC')    ,applyto='die'    )
+name='dout 06h'             ; dout_06h                 = Operation.create(name=name  ,seq=Nop(ExecTime[name])                               + End(name)          ,applyto='die'    )
+name='SR 70h'               ; SR_70h                   = Operation.create(name=name  ,seq=Nop(ExecTime[name])                               + End(name)          ,applyto='die'    )
+name='SR 78h'               ; SR_78h                   = Operation.create(name=name  ,seq=Nop(ExecTime[name])                               + End(name)          ,applyto='die'    )
+name='reset FAh'            ; reset_FAh                = Operation.create(name=name  ,seq=Nop(ExecTime[name]) + tRST_ready                  + End(name)          ,applyto='die'    )
+name='erase TLC suspend'    ; erase_TLC_suspend        = Operation.create(name=name  ,seq=Nop(ExecTime[name]) + tERSL_TLC                   + End(name)          ,applyto='die'    )
+name='erase TLC resume'     ; erase_TLC_resume         = Operation.create(name=name  ,seq=Nop(ExecTime[name]) + tERS_TLC.shift_time(-2*MS)  + End(name)          ,applyto='die'    )
+name='program TLC suspend'  ; program_TLC_suspend      = Operation.create(name=name  ,seq=Nop(ExecTime[name]) + tERSL_TLC                   + End(name)          ,applyto='die'    )
+name='program TLC resume'   ; program_TLC_resume       = Operation.create(name=name  ,seq=Nop(ExecTime[name]) + tPROGO.shift_time(-300*US)  + End(name)          ,applyto='die'    )
